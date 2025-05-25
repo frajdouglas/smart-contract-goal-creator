ec72649b36a317fb8ea193c7963ba16fc977bfa9
Fraser Douglas <87184070+frajdouglas@users.noreply.github.com>
2025-05-25 16:22:01 +0000

    update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,47 +28,10 @@ export function Navbar() {
           <Link href="/referee" className="text-sm font-medium hover:text-primary">
             Referee
           </Link>
+          <Link href="/claim-escrow" className="text-sm font-medium hover:text-primary">
+            Claim Escrow
+          </Link>
         </nav>
-
-        <div className="flex items-center space-x-4">
-          {user ? (
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-                  <Avatar className="h-8 w-8">
-                    <AvatarFallback>{user.email?.[0].toUpperCase() || "U"}</AvatarFallback>
-                  </Avatar>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem asChild>
-                  <Link href="/profile" className="flex items-center">
-                    <User className="mr-2 h-4 w-4" />
-                    Profile
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem asChild>
-                  <Link href="/goals" className="flex items-center">
-                    <Goal className="mr-2 h-4 w-4" />
-                    My Goals
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => signOut()} className="flex items-center">
-                  <LogOut className="mr-2 h-4 w-4" />
-                  Sign Out
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          ) : (
-            <Button asChild variant="outline" size="sm">
-              <Link href="/">Sign In</Link>
-            </Button>
-          )}
-
-          <Button asChild size="sm">
-            <Link href="/create">Create Goal</Link>
-          </Button>
-        </div>
       </div>
     </header>
   )
