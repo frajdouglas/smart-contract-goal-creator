fe7ec30f04afaa7660e8d2cf538ab7b1914bec30
Fraser Douglas <87184070+frajdouglas@users.noreply.github.com>
2025-05-25 15:44:36 +0000

    tidying

--- lib/api/markGoalAsComplete.ts
+++ lib/api/markGoalAsComplete.ts
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-interface CreateGoalResponse {
-    message: string;
-};
-
 export async function markGoalAsComplete(contract_goal_id: number, transactionHash: string): Promise<CreateGoalResponse> {
     try {
         const response = await axios.post<{ contract_goal_id: number; transactionHash: string }>(
