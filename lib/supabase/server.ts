168e3d565bca0759fd2071316199b4cce74f2bb2
Fraser Douglas <87184070+frajdouglas@users.noreply.github.com>
2025-05-27 09:47:18 +0000

    removes hard coding of key

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,9 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-// const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
-// console.log( supabaseServiceRoleKeyenv, "supabaseServiceRoleKeyenv");    
-const supabaseServiceRoleKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5c3JxdmJubGttcXlvaGJueWlqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0Njc4MzcyNSwiZXhwIjoyMDYyMzU5NzI1fQ.yr82rJUfn5meDOaleUx2clDqJv8TOEOUkmCVrEjINLI'
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
 if (!supabaseServiceRoleKey) {
   throw new Error("SUPABASE_SERVICE_ROLE_KEY is not defined in the environment variables.");
 }
