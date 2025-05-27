e517a2367879f295d19f66e5e7ae43b052513dc8
Fraser Douglas <87184070+frajdouglas@users.noreply.github.com>
2025-05-27 11:00:10 +0000

    resolves warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-"use client"; // Make sure this is at the top if any of its direct children use client-side features
+"use client";
+
 import type React from "react"
-import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -11,23 +11,18 @@ import { MetaMaskProvider } from "@metamask/sdk-react"
 
 const inter = Inter({ subsets: ["latin"] })
 
-// export const metadata: Metadata = {
-//   title: "Goal Accountability Tracker",
-//   description: "Set goals, stake ETH, and achieve more with blockchain-powered accountability",
-//     generator: 'v0.dev'
-// }
+const dappUrl = typeof window !== "undefined"
+  ? `${window.location.protocol}//${window.location.host}`
+  : "https://default-dapp-url.com";
 
-const host =
-    typeof window !== "undefined" ? window.location.host : "defaultHost";
-console.log(host)
-  const sdkOptions = {
-    logging: { developerMode: false },
-    checkInstallationImmediately: false,
-    dappMetadata: {
-      name: "Next-Metamask-Boilerplate",
-      url: host, // using the host constant defined above
-    },
-  };
+const sdkOptions = {
+  logging: { developerMode: false },
+  checkInstallationImmediately: false,
+  dappMetadata: {
+    name: "Next-Metamask-Boilerplate",
+    url: dappUrl,
+  },
+};
 
 export default function RootLayout({
   children,
@@ -35,15 +30,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <MetaMaskProvider sdkOptions={sdkOptions} debug={false}>
-          <AuthProvider>
-            <Navbar />
-            <main>{children}</main>
-            <Toaster />
-          </AuthProvider>
+            <AuthProvider>
+              <Navbar />
+              <main>{children}</main>
+              <Toaster />
+            </AuthProvider>
           </MetaMaskProvider>
         </ThemeProvider>
       </body>
